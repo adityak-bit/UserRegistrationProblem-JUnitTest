a599ae9346a60faed1494f8b4595a9a32ed33267
adityak-bit <adityajsr1998@gmail.com>
2020-10-05 18:46:51 +0530

    [ADITYA] Added email id validator

--- src/main/java/com/capgemini/urpjt/UserRegistration.java
+++ src/main/java/com/capgemini/urpjt/UserRegistration.java
@@ -9,7 +9,7 @@ public class UserRegistration {
 	Scanner sc = new Scanner(System.in);
 	
 	public void firstNameValidator() {
-		System.out.println("Enter your First Nmae");
+		System.out.println("Enter your First Name");
 		System.out.println("First Name should start with uppercase and have min 3 characters");
 		String firstName = sc.nextLine();
 		Pattern pattern = Pattern.compile("^([A-Z])[A-Za-z]{2,}");
@@ -21,10 +21,39 @@ public class UserRegistration {
 			System.out.println("Please enter correct First Name");
 	}
 	
+	public void lastNameValidator() {
+		System.out.println("Enter your Last Name");
+		System.out.println("Last Name should start with uppercase and have min 3 characters");
+		String lastName = sc.nextLine();
+		Pattern pattern = Pattern.compile("^([A-Z])[A-Za-z]{2,}");
+		Matcher matcher = pattern.matcher(lastName);
+		boolean b = matcher.find();
+		if (b)
+			System.out.println("Last Name is validated");
+		else
+			System.out.println("Please enter correct Last Name");
+	}
+	
+	public void emailIdValidator() {
+		System.out.println("Enter your email id");
+		String emailId = sc.nextLine();
+		Pattern pattern = Pattern.compile("^([a-z]){1,}[a-z0-9]*([+_-]){0,1}[a-z0-9]+(@)([a-z0-9]+)(\\.[a-z]{2,3}){1}(\\.[a-z]{2})?$");
+		Matcher matcher = pattern.matcher(emailId);
+		boolean b = matcher.find();
+		if (b)
+			System.out.println("Email Id is validated");
+		else
+			System.out.println("Please enter correct Email Id");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration page");
 		UserRegistration newUser = new UserRegistration();
 
-		newUser.firstNameValidator();
+		//newUser.firstNameValidator();//first name validator
+		
+		//newUser.lastNameValidator();//last name validator
+		
+		newUser.emailIdValidator();//email id validator
 	}
 }
